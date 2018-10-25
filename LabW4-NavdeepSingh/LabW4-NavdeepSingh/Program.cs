e46439589511c03730c7c6076023de899d56a77f
NSingh900 <43621904+NSingh900@users.noreply.github.com>
2018-10-24 20:41:15 -0400

    Add files via upload

---
+++ LabW4-NavdeepSingh/LabW4-NavdeepSingh/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW4_NavdeepSingh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Enter 2 integers");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int total = x + y;
+            Console.WriteLine(total);
+            */
+
+            /*
+            Console.WriteLine("Output to Solution");
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            int (total) = (x + y)(z + 10);
+            Console.WriteLine(total);
+            */
+
+            /*
+            {
+                Console.WriteLine("Hello\tWorld!");
+            }
+            */
+
+            /*
+            {
+                Console.WriteLine("Hello\nWorld");
+            }
+            */
+
+            /*
+            {
+                Console.WriteLine("\"Hello World!\"");
+            }
+            */
+
+            /*
+            {
+                Console.WriteLine("Hello\\World");
+            }
+            */
+        }
+    }
+}
